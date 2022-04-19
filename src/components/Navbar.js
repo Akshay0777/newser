cef84d0ed321aa46c90b7dfdd7038f0dfe721809
Akshay0777 <41819302+Akshay0777@users.noreply.github.com>
2022-04-19 16:14:15 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
         <div>
             <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <Link class="navbar-brand" to="/">Newser</Link>
+                    <Link class="navbar-brand" to="/">Newsser</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
